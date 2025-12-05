68be5072c61b2d7ad881b6ca1597ffc4b051d488
abhishekkumar <send2abhishek@live.com>
2025-12-05 22:43:37 +0530

    feat: Add prompt templates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from langchain_ollama import ChatOllama
 from langchain_core.messages import SystemMessage,HumanMessage
+from langchain_core.prompts import (SystemMessagePromptTemplate,HumanMessagePromptTemplate,PromptTemplate,ChatPromptTemplate)
 
 llm = ChatOllama(
     model="llama3.1:latest",
@@ -9,11 +10,19 @@ llm = ChatOllama(
     # other params ...
 )
 
-messages = [
-            SystemMessage(content="You are a helpful geography assistant! Your name is Lina."),
-            HumanMessage(content="Why sky is cloudy? what did it denotes? "),
-        ]
+# messages = [
+#             SystemMessage(content="You are a helpful geography assistant! Your name is Lina."),
+#             HumanMessage(content="Why sky is cloudy? what did it denotes? "),
+#         ]
 
-answer = llm.invoke(messages)
 
-print(answer.content)
+system = SystemMessagePromptTemplate.from_template('You are helpful {stream} assistant! Your name is {name}. ')
+question = HumanMessagePromptTemplate.from_template('Why sky is cloudy? what did it denotes in {points} points')
+#
+system.format(stream='geography', name='Laura')
+question.format(points=3)
+
+template = ChatPromptTemplate([system, question])
+questionAns = template.invoke({'stream': 'geography', 'name': 'Laura', 'points': 5})
+
+print(llm.invoke(questionAns).content)
